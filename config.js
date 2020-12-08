5527f31be92eac7e2b46aabf5c59ebeb1b30669f
Calvin Qi <calvin.qi@sambanovasystems.com>
2020-12-07 17:57:05 -0800

    Add real latex matrix tool, many other files

---
+++ config.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  url: 'asdf.asdf.fun',
+  pathPrefix: '/',
+  title: 'calvinqi.com',
+  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  copyright: '© Calvin Qi 2020',
+  disqusShortname: '',
+  postsPerPage: 6,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Blog',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    // {
+    //   label: 'Contact me',
+    //   path: '/pages/contacts'
+    // },
+    {
+      label: 'Tags',
+      path: '/tags/'
+    }
+  ],
+  author: {
+    name: 'Calvin Qi',
+    photo: '/photo.jpg',
+    bio: 'Big bio here.',
+    contacts: {
+      email: 'calvincqi@gmail.com',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'calvinqi',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'calvinqi',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
---
+++ content/posts/Latex-Matrix-Tool.md
@@ -0,0 +1,17 @@
+---
+title: LaTeX Matrix tool
+date: "2018-11-19T22:40:32.169Z"
+template: "post"
+draft: false
+slug: "latex-matrix-tool"
+category: "Widgets"
+tags:
+  - "Widget"
+  - "Math"
+description: "Interactive tool for quickly constructing a matrix and automatically generating the LaTeX source code (because typing out matrices in LaTeX gets annoying)"
+socialImage: "/media/42-line-bible.jpg"
+---
+<div>
+<iframe src="/widgets/matrix" width="100%" height="700px" frameBorder="0">
+</iframe>
+<div>
