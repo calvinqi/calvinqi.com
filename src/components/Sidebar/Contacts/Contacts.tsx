369ce5073cf587d7e53a76928ed56fb4fd0f8e13
calvinqi <calvincqi@gmail.com>
2022-12-29 22:05:27 -0800

    add email contact and image

--- src/components/Sidebar/Contacts/Contacts.tsx
+++ src/components/Sidebar/Contacts/Contacts.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { atob } from "abab";
+// import { atob } from "abab";
 
 import { Icon } from "@/components/Icon";
 import { ICONS } from "@/constants";
@@ -17,30 +17,18 @@ const Contacts: React.FC<Props> = ({ contacts }: Props) => (
   <div className={styles.contacts}>
     <ul className={styles.list}>
       {(Object.keys(contacts) as Array<keyof typeof ICONS>).map((name) =>
-        contacts[name] ? (
+        contacts[name] && (
           <li className={styles.item} key={name}>
-            {name === "email" ? (
-              <span
-                className={styles.link}
-                onClick={() => {
-                  window.location.href =
-                    "mailto:" + atob(getContactHref(name, contacts[name]));
-                }}
-              >
-                <Icon name={name} icon={getIcon(name)} />
-              </span>
-            ) : (
-              <a
-                className={styles.link}
-                href={getContactHref(name, contacts[name])}
-                target="_blank"
-                rel={`noopener noreferrer${name === "mastodon" ? " me" : ""}`}
-              >
-                <Icon name={name} icon={getIcon(name)} />
-              </a>
-            )}
+            <a
+              className={styles.link}
+              href={name === "email" ? "/email.svg" : getContactHref(name, contacts[name])}
+              target="_blank"
+              rel={`noopener noreferrer${name === "mastodon" ? " me" : ""}`}
+            >
+              <Icon name={name} icon={getIcon(name)} />
+            </a>
           </li>
-        ) : null,
+        ),
       )}
     </ul>
   </div>
